b67f06324d80a0b3f3495df1ddb6dc13c9e9dfd2
CarlJazper <perezcarljazper+github@gmail.com>
2025-05-16 18:54:05 +0800

    u

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,7 +1,7 @@
 // src/utils/baseUrl.js
 const baseURL =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === 'production'
     ? 'http://localhost:8080/api/v1' // or your actual local IP/server
-    : process.env.NEXT_PUBLIC_API_BASE_URL || 'https://pspweb-backend.onrender.com'; // fallback for prod
+    : process.env.NEXT_PUBLIC_API_BASE_URL || 'https://pspweb-backend.onrender.com/api/v1'; // fallback for prod
 
 export default baseURL;
