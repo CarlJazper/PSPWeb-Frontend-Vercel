a4125287d720df17153c3efcfd99354545d02e32
CarlJazper <perezcarljazper+github@gmail.com>
2025-06-27 01:33:13 +0800

    ups

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,8 +1,8 @@
 const baseURL = import.meta.env.PROD
-  // ? 'https://pspweb-backend.onrender.com/api/v1'
-  // : 'https://pspweb-backend.onrender.com/api/v1';
-  ? 'http://localhost:8000/api/v1'
-  : 'http://localhost:8000/api/v1';
+  ? 'https://pspweb-backend.onrender.com/api/v1'
+  : 'https://pspweb-backend.onrender.com/api/v1';
+  // ? 'http://localhost:8000/api/v1'
+  // : 'http://localhost:8000/api/v1';
 // const baseURL = 'http://192.168.100.108:8080/api/v1';
 
 export default baseURL;
